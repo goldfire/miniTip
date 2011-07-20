7d84f04429600452de4f215cbd151dc519dd622b
goldfire <james33@gmail.com>
2011-07-20 00:28:47 +0000

    Updated outline

--- jquery.miniTip.js
+++ jquery.miniTip.js
@@ -1,17 +1,19 @@
- /*
- * miniTip
- * Copyright 2011 James Simpson
- * goldfirestudios.com
- * 
+/*!
+ * jQuery miniTip plugin v0.0.2
  *
- * Version 0.1   -   Updated: July 19, 2011
+ * Updated: July 19, 2011
+ * Requires: jQuery v1.5+
  *
- * Description....
+ * Copyright 2011, James Simpson
+ * http://goldfirestudios.com
  *
- * This miniTip jQuery plug-in is dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- */
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Documentation can be found at http://goldfirestudios.com/
+ *
+*/
 
 (function($){
     $.fn.miniTip = function(options) {
