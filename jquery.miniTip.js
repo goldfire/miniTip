0d9324612f40aa38d5be79c1300239ac211f1fec
goldfire <james33@gmail.com>
2011-07-20 00:17:30 +0000

    Make base file for plugin

---
+++ jquery.miniTip.js
@@ -0,0 +1,20 @@
+ /*
+ * miniTip
+ * Copyright 2011 James Simpson
+ * goldfirestudios.com
+ * 
+ *
+ * Version 0.1   -   Updated: July 19, 2011
+ *
+ * Description....
+ *
+ * This miniTip jQuery plug-in is dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+(function($){
+    $.fn.miniTip = function(options) {
+
+    }
+})(jQuery);
